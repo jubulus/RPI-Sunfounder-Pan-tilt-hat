89567d601e56a46f6e2fd4e8bcc378cd9a941d2f
jubulus <91372642+jubulus@users.noreply.github.com>
2021-09-25 13:10:45 +0200

    Create Face_Tracking.py
    Need Ezblock librairies from sunfounder github or an image OS package install from https://ezblock.cc/download/index.html

---
+++ Face_Tracking.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# Need Ezblock librairies from sunfounder github or an image os install from https://ezblock.cc/download/index.html
+
+import sys
+sys.path.append(r'/opt/ezblock')
+from vilib import Vilib
+from ezblock import WiFi
+from ezblock import constrain
+from ezblock import PWM
+from ezblock import Servo
+
+xVal = None
+yVal = None
+panAngle = None
+tiltAngle = None
+
+Vilib.camera_start(True)
+Vilib.human_detect_switch(True)
+panAngle = 0
+tiltAngle = 0
+WiFi().write('Country', 'ssid', 'ssidPass')
+
+pwm_P0 = PWM("P0")
+
+pwm_P1 = PWM("P1")
+
+
+def forever():
+    global xVal, yVal, panAngle, tiltAngle
+    xVal = Vilib.human_detect_object('x')
+    yVal = Vilib.human_detect_object('y')
+    if (Vilib.human_detect_object('number')) >= 1:
+        if xVal == -1:
+            panAngle = panAngle + 1
+        elif xVal == 1:
+            panAngle = panAngle - 1
+        if yVal == -1:
+            tiltAngle = tiltAngle + 1
+        elif yVal == 1:
+            tiltAngle = tiltAngle - 1
+        panAngle = constrain(panAngle, -90, 90)
+        tiltAngle = constrain(tiltAngle, -45, 25)
+        Servo(pwm_P0).angle(panAngle)
+        Servo(pwm_P1).angle(tiltAngle)
+
+if __name__ == "__main__":
+    while True:
+        forever()  
